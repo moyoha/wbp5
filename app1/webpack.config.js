d0e665eb2dfeed0fbab7452e0d0e51fc67a4de19
v_mayonghua <v_mayonghua@baidu.com>
2024-04-09 19:53:59 +0800

    初步完成架构

--- app1/webpack.config.js
+++ app1/webpack.config.js
@@ -8,5 +8,22 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "./dist"),
         pubilcPath: "http://localhost:8081/dist/",
-    }
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            // MF 应用名称
+            name: "app1",
+            // MF 模块入口，可以理解为该应用的资源清单
+            filename: "remoteEntry.js",
+            exposes: {
+                "./utils": "./src/utils"
+            },
+        })
+    ],
+    // MF 应用资源提供方必须以 http(s) 形式提供服务
+    // 所以这里需要使用 devServer 提供 http(s) server 能力
+    devServer: {
+        port: 8081,
+        hot: true,
+    },
 }
